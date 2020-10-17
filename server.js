7ef73d76821170d3f6ef773539f0d73097a4815b
Clyde Clark <clydeclark78@gmail.com>
2020-10-17 13:20:58 -0700

    routes fixed in server.js

--- server.js
+++ server.js
@@ -19,9 +19,10 @@ app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
 var routes = require("./routes/post-route.js");
-var routes = require("./routes/seller-route.js");
+var routes2 = require("./routes/seller-route.js");
 
 app.use(routes);
+app.use(routes2);
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
